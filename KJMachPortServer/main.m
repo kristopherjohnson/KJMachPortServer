d0bc6c4c07bed6dbc39ee1e228054cf314c5a6ae
Kristopher Johnson <kris@kristopherjohnson.net>
2017-06-29 18:48:09 -0400

    Initial Commit

---
+++ KJMachPortServer/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  KJMachPortServer
+//
+//  Created by Kristopher Johnson on 6/29/17.
+//  Copyright © 2017 Kristopher Johnson. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
